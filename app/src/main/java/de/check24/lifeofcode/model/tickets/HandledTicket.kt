ec632653661be055aacf35daa48edbb0783a704c
Björn Kopiske <b.kopiske@iplabs.de>
2022-10-23 15:15:26 +0200

    Add first unit and fix production code

--- app/src/main/java/de/check24/lifeofcode/model/tickets/HandledTicket.kt
+++ app/src/main/java/de/check24/lifeofcode/model/tickets/HandledTicket.kt
@@ -1,7 +1,9 @@
 package de.check24.lifeofcode.model.tickets
 
+import de.check24.lifeofcode.model.work.Week
+
 data class HandledTicket(
     val ticket: Ticket,
-    val implementationStartDate: Date,
-    val readyForVerificationDate: Date
+    val implementationStartDate: Week,
+    val readyForVerificationDate: Week
 )
